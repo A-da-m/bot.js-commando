3ee92e1645cdf90b9d27d9ad84395a41c4745dd9
abcdan <dahnjohl@gmail.com>
2018-08-14 22:27:31 +0200

    Fixed typo

--- blanks/command.js
+++ blanks/command.js
@@ -2,7 +2,7 @@ var main = require("../main.js")
 var config = require("../config.json")
 
 exports.run = (message, args) => {
-    // Put code of bot here
+    // Put code of command here
 }
 
 exports.isPublic = true
