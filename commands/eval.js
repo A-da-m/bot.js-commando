9cd089a0fa6a8fc056ce7e648fa49e979efaeaaf
abcdan <dahnjohl@gmail.com>
2018-08-14 22:32:49 +0200

    Fixed eval

--- commands/eval.js
+++ commands/eval.js
@@ -1,18 +1,23 @@
 var main = require("../main.js")
 var config = require("../config.json")
 
-exports.run = (message,args) => {
-  if(message.author.id === main.config.owner){
-    var code = message.content.match(/\s.*/)[0].match(/\w.*/)[0]
-    try{
-      var evaled = eval(code);
-      message.channel.send(evaled)
-    }catch(err){
-        message.channel.send(`:x: ERROR \`\`\`xl\n${err}\n\`\`\``);
+exports.run = (message, args) => {
+    if (message.author.id === main.config.owner) {
+        var id = message.content.split(" ")[1]
+        if (id == null || id == "") {
+            message.channel.send(`Tell me what I have to eval... \`${config.prefix}eval message.channel.send("?")\``)
+        } else {
+            var code = message.content.match(/\s.*/)[0].match(/\w.*/)[0]
+            try {
+                var evaled = eval(code);
+                message.channel.send(evaled)
+            } catch (err) {
+                message.channel.send(`\`ERROR\` \`\`\`xl\n${err}\n\`\`\``);
+            }
+        }
+    } else {
+        message.reply(":x: This command can only be used by the person who owns me.")
     }
-  }else{
-    message.reply(":x: This command can only be used by the person who owns me.")
-  }
 }
 
 exports.isPublic = true
