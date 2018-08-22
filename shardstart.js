05673771e06bda38e6ebb71ad71e9a4639e2024d
abcdan <dahnjohl@gmail.com>
2018-08-22 17:50:32 +0200

    :bug: Forgot to add colors

--- shardstart.js
+++ shardstart.js
@@ -1,5 +1,6 @@
 const { ShardingManager }= require('discord.js');
 const config = require('./settings/config.json');
+const colors = require('colors');
 const manager = new ShardingManager('./bot.js', { 
     token: config.token,
     autoSpawn: true
@@ -8,4 +9,4 @@ const manager = new ShardingManager('./bot.js', {
 manager.spawn();                    // Put a number between the () if you want a certain amount of shards to spawn.
                                     // This will start the bot with the recommended amount of shards that Discord will return.
 
-manager.on('launch', shard => console.log(`[SHARD] Launched shard ${shard.id}`));
+manager.on('launch', shard => console.log(`[BOT] Launched shard ${shard.id}`.magenta));
